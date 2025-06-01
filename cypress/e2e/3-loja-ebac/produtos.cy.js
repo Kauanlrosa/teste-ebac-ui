972b47c79b6371d02421d626cf556d72c44fe9c6
Kauan Rosa <kauan220017@gmail.com>
2025-05-31 21:41:45 -0300

    Continuação das aulas

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,38 @@
 /// <reference types="cypress"/>
 
+import produtosPage from "../../support/page-objects/produtos.page";
+
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
-    it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-            //.first()
-            //.last()
-            //.eq(2)
-            .contains('Ajax Full-Zip Sweatshirt')
-            .click()
-        
+        it('Deve selecionar um produto da lista', () => {
+        produtosPage.buscarProdutoLista('Beaumont Summit Kit')
             cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
+
+        it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+
+    });
+
+        it('Deve visitar a pagina do produto', () => {
+
+
+
+
+    });
+
+
+    it('Deve adicionar o produto ao carrinho', () => {
+
+
+
+        
+    });
+
 });
